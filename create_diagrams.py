784717d5d882e2b307ce7207f28e3ee54e1df1f0
mole99 <leomoser99@gmail.com>
2024-02-28 08:17:12 +0100

    Update diagrams, use min/max for error

--- create_diagrams.py
+++ create_diagrams.py
@@ -14,6 +14,8 @@ sea_of_gates_measurements = {'fabric_small' : [], 'fabric_medium' : [], 'fabric_
 fabric_stitching_measurements = {'fabric_small' : [], 'fabric_medium' : [], 'fabric_large' : []}
 harden_tiles_measurements = []
 
+colors = ['#22d2f7', '#e3607a', '#14d301']
+
 def load_measurements():
 
     assert(os.path.isdir('measurements/harden_tiles/'))
@@ -46,7 +48,7 @@ def load_measurements():
 def bar_chart_time(samples=3):
     # set width of bar 
     barWidth = 0.25
-    fig, ax = plt.subplots(figsize=(15*0.8,7*0.8)) 
+    fig, ax = plt.subplots(figsize=(15*0.4,7*0.4)) 
 
     # Calculate mean value and stddev
     time_sog = {'fabric_small': [], 'fabric_medium': [], 'fabric_large': []}
@@ -77,51 +79,68 @@ def bar_chart_time(samples=3):
     data_harden_tiles = [np.mean(time_tiles)]*3
     
     # Error
-    error_sea_of_gates = [
-        np.std(time_sog['fabric_small']),
-        np.std(time_sog['fabric_medium']),
-        np.std(time_sog['fabric_large'])
-    ]
-    error_fabric_stitching = [
-        np.std(time_stitch['fabric_small']),
-        np.std(time_stitch['fabric_medium']),
-        np.std(time_stitch['fabric_large'])
-    ]
-    error_harden_tiles = [np.std(time_tiles)]*3
+    error_sea_of_gates_low = [abs(min([time - data_sea_of_gates[i] for time in time_sog[fabric]])) for i, fabric in enumerate(fabric_sizes)]
+    error_sea_of_gates_high = [max([time - data_sea_of_gates[i] for time in time_sog[fabric]]) for i, fabric in enumerate(fabric_sizes)]
+    
+    error_fabric_stitching_low = [abs(min([time - data_fabric_stitching[i] for time in time_stitch[fabric]])) for i, fabric in enumerate(fabric_sizes)]
+    error_fabric_stitching_high = [max([time - data_fabric_stitching[i] for time in time_stitch[fabric]]) for i, fabric in enumerate(fabric_sizes)]
+    
+    error_harden_tiles_low = [abs(min([time - data_harden_tiles[i] for time in time_tiles])) for i, fabric in enumerate(fabric_sizes)]
+    error_harden_tiles_high = [max([time - data_harden_tiles[i] for time in time_tiles]) for i, fabric in enumerate(fabric_sizes)]
 
     print(data_sea_of_gates)
     print(data_fabric_stitching)
     print(data_harden_tiles)
     
-    print(error_sea_of_gates)
-    print(error_fabric_stitching)
-    print(error_harden_tiles)
+    print(error_sea_of_gates_low)
+    print(error_fabric_stitching_low)
+    print(error_harden_tiles_low)
+    
+    print(error_sea_of_gates_high)
+    print(error_fabric_stitching_high)
+    print(error_harden_tiles_high)
 
     # Set position of bar on X axis 
     br1 = np.arange(len(data_sea_of_gates)) 
     br2 = [x + barWidth for x in br1]
 
-    colors = ['#23d6fc', '#e3607a', '#14d301']
-
     # Make the plot
     plt.bar(br1, data_sea_of_gates, color=colors[0], width=barWidth,
-            edgecolor='black', label='Sea of Gates', yerr=error_sea_of_gates)
+            edgecolor='black', label='Sea of Gates', yerr=(error_sea_of_gates_low, error_sea_of_gates_high))
 
     plt.bar(br2, data_harden_tiles, color=colors[1], width=barWidth,
-            edgecolor='black', label='Tiles Hardening', yerr=error_harden_tiles)
+            edgecolor='black', label='Tiles Hardening', yerr=(error_harden_tiles_low, error_harden_tiles_high))
 
     plt.bar(br2, data_fabric_stitching, color=colors[2], width=barWidth,
-            edgecolor='black', label='Fabric Stitching', yerr=error_harden_tiles, bottom=data_harden_tiles) 
+            edgecolor='black', label='Fabric Stitching', yerr=(error_harden_tiles_low, error_harden_tiles_high), bottom=data_harden_tiles) 
 
-    # Adding Xticks
-    plt.ylabel('Time in Minutes', fontweight ='bold', fontsize = 15) 
-    plt.xticks([r + barWidth/2 for r in range(len(data_sea_of_gates))], 
-            ['Small 1x1 CLB', 'Medium 5x5 CLB', 'Large 10x10 CLB'])
-     
-    ax.legend(loc='upper left', fontsize="14", fancybox=True)
+    # inset axes....
+    x1, x2, y1, y2 = -barWidth, barWidth*6, 0, 25  # subregion of the original image
+    axins = ax.inset_axes([0.15, 0.2, 0.4, 0.4], xlim=(x1, x2), ylim=(y1, y2), xticklabels=[])
+
+    axins.bar(br1, data_sea_of_gates, color=colors[0], width=barWidth,
+            edgecolor='black', label='Sea of Gates', yerr=(error_sea_of_gates_low, error_sea_of_gates_high))
+    axins.bar(br2, data_harden_tiles, color=colors[1], width=barWidth,
+            edgecolor='black', label='Tiles Hardening', yerr=(error_harden_tiles_low, error_harden_tiles_high))
+
+    ax.indicate_inset_zoom(axins, edgecolor="black")
+
+    axins.set_xticks([])
+    axins.grid()
     
     # this locator puts ticks at regular intervals
     loc = plticker.MultipleLocator(base=10.0)
+    axins.yaxis.set_major_locator(loc)
+
+    # Adding Xticks
+    plt.ylabel('Time in Minutes')
+    plt.xticks([r + barWidth/2 for r in range(len(data_sea_of_gates))], 
+            ['Small 1x1 CLB', 'Medium 5x5 CLB', 'Large 10x10 CLB'])
+     
+    ax.legend(loc='upper left', fancybox=True)#, fontsize="10")
+    
+    # this locator puts ticks at regular intervals
+    loc = plticker.MultipleLocator(base=40.0)
     ax.yaxis.set_major_locator(loc)
     
     ax.set_axisbelow(True)
@@ -137,7 +156,7 @@ def bar_chart_ram(samples=3):
 
     # set width of bar 
     barWidth = 0.25
-    fig, ax = plt.subplots(figsize=(15*0.8,7*0.8)) 
+    fig, ax = plt.subplots(figsize=(15*0.4,7*0.4)) 
 
     # Calculate mean value and stddev
     ram_sog = {'fabric_small': [], 'fabric_medium': [], 'fabric_large': []}
@@ -164,49 +183,48 @@ def bar_chart_ram(samples=3):
     data_harden_tiles = [np.mean(ram_tiles)]*3
     
     # Error
-    error_sea_of_gates = [
-        np.std(ram_sog['fabric_small']),
-        np.std(ram_sog['fabric_medium']),
-        np.std(ram_sog['fabric_large'])
-    ]
-    error_fabric_stitching = [
-        np.std(ram_stitch['fabric_small']),
-        np.std(ram_stitch['fabric_medium']),
-        np.std(ram_stitch['fabric_large'])
-    ]
-    error_harden_tiles = [np.std(ram_tiles)]*3
+    error_sea_of_gates_low = [abs(min([ram - data_sea_of_gates[i] for ram in ram_sog[fabric]])) for i, fabric in enumerate(fabric_sizes)]
+    error_sea_of_gates_high = [max([ram - data_sea_of_gates[i] for ram in ram_sog[fabric]]) for i, fabric in enumerate(fabric_sizes)]
+    
+    error_fabric_stitching_low = [abs(min([ram - data_fabric_stitching[i] for ram in ram_stitch[fabric]])) for i, fabric in enumerate(fabric_sizes)]
+    error_fabric_stitching_high = [max([ram - data_fabric_stitching[i] for ram in ram_stitch[fabric]]) for i, fabric in enumerate(fabric_sizes)]
+    
+    error_harden_tiles_low = [abs(min([ram - data_harden_tiles[i] for ram in ram_tiles])) for i, fabric in enumerate(fabric_sizes)]
+    error_harden_tiles_high = [max([ram - data_harden_tiles[i] for ram in ram_tiles]) for i, fabric in enumerate(fabric_sizes)]
     
     print(data_sea_of_gates)
     print(data_fabric_stitching)
     print(data_harden_tiles)
     
-    print(error_sea_of_gates)
-    print(error_fabric_stitching)
-    print(error_harden_tiles)
+    print(error_sea_of_gates_low)
+    print(error_fabric_stitching_low)
+    print(error_harden_tiles_low)
+    
+    print(error_sea_of_gates_high)
+    print(error_fabric_stitching_high)
+    print(error_harden_tiles_high)
     
     # Set position of bar on X axis 
     br1 = np.arange(len(data_sea_of_gates)) 
     br2 = [x + barWidth for x in br1]
     br3 = [x + barWidth for x in br2]
 
-    colors = ['#23d6fc', '#e3607a', '#14d301']
-
     # Make the plot
-    plt.bar(br1, data_sea_of_gates, color = colors[0], width = barWidth, 
-            edgecolor ='black', label ='Sea of Gates', yerr = error_sea_of_gates)
+    ax.bar(br1, data_sea_of_gates, color = colors[0], width = barWidth, 
+            edgecolor ='black', label ='Sea of Gates', yerr = [error_sea_of_gates_low, error_sea_of_gates_high])
             
-    plt.bar(br2, data_harden_tiles, color = colors[1], width = barWidth, 
-            edgecolor ='black', label ='Tiles Hardening', yerr = error_harden_tiles)
+    ax.bar(br2, data_harden_tiles, color = colors[1], width = barWidth, 
+            edgecolor ='black', label ='Tiles Hardening', yerr = (error_harden_tiles_low, error_harden_tiles_high))
             
-    plt.bar(br3, data_fabric_stitching, color = colors[2], width = barWidth, 
-            edgecolor ='black', label ='Fabric Stitching', yerr = error_fabric_stitching) 
-     
+    ax.bar(br3, data_fabric_stitching, color = colors[2], width = barWidth, 
+            edgecolor ='black', label ='Fabric Stitching', yerr = (error_fabric_stitching_low, error_fabric_stitching_high))
+
     # Adding Xticks
-    plt.ylabel('RAM Usage in GiB', fontweight ='bold', fontsize = 15) 
+    plt.ylabel('RAM Usage in GiB') 
     plt.xticks([r + barWidth for r in range(len(data_sea_of_gates))], 
             ['Small 1x1 CLB', 'Medium 5x5 CLB', 'Large 10x10 CLB'])
 
-    ax.legend(loc='upper left', fontsize="14", fancybox=True)
+    ax.legend(loc='upper left', fancybox=True)
     
     # this locator puts ticks at regular intervals
     loc = plticker.MultipleLocator(base=1.0)
@@ -252,7 +270,7 @@ def diagram_batch(samples=3):
                 data = json.load(f)
                 batch_measurements[key].append(data.copy())
     
-    all_data_time = [ [batch_measurements[key][-sample-1]['elapsed_time_perf_counter'] for sample in range(samples)] for key in batch_measurements]
+    all_data_time = [ [batch_measurements[key][-sample-1]['elapsed_time_perf_counter'] / 60 for sample in range(samples)] for key in batch_measurements]
     all_data_ram = [ [batch_measurements[key][-sample-1]['max_memory']['RUSAGE_CHILDREN'] / (1024**2) for sample in range(samples)] for key in batch_measurements]
     all_data_area = [ [round(float(batch_measurements[key][-sample-1]['design__die__bbox'].split(' ')[2])**2 / 1000_000, 2) for sample in range(samples)] for key in batch_measurements]
 
@@ -265,59 +283,85 @@ def diagram_batch(samples=3):
     data_area = [np.mean(data_array) for data_array in all_data_area]
     
     error_time = [np.std(data_array) for data_array in all_data_time]
+    
+    error_time_low = [abs(min([data - data_time[i] for data in data_array])) for i, data_array in enumerate(all_data_time)]
+    error_time_high = [max([data - data_time[i] for data in data_array]) for i, data_array in enumerate(all_data_time)]
+
     error_ram  = [np.std(data_array) for data_array in all_data_ram]
+    
+    error_ram_low = [abs(min([data - data_ram[i] for data in data_array])) for i, data_array in enumerate(all_data_ram)]
+    error_ram_high = [max([data - data_ram[i] for data in data_array]) for i, data_array in enumerate(all_data_ram)]
+    
     error_area = [np.std(data_array) for data_array in all_data_area]
+
+    # No change in area possible, else floating porint imprecision
+    error_area_low = [0]*len(data_area)#[abs(min([data - data_area[i] for data in data_array])) for i, data_array in enumerate(all_data_area)]
+    error_area_high = [0]*len(data_area)#[max([data - data_area[i] for data in data_array]) for i, data_array in enumerate(all_data_area)]
 
     print(data_time)
     print(data_ram)
     print(data_area)
     
-    print(error_time)
-    print(error_ram)
-    print(error_area)
+    print(error_time_low)
+    print(error_ram_low)
+    print(error_area_low)
+    
+    print(error_time_high)
+    print(error_ram_high)
+    print(error_area_high)
 
     keys = [key for key in batch_measurements]
 
-    fig, ax1 = plt.subplots(figsize=(15*0.8,7*0.8)) 
-    fig.subplots_adjust(right=0.75)
+    fig, ax = plt.subplots(3, figsize=(15*0.4,7*0.7)) 
 
-    colors = ['#23d6fc', '#e3607a', '#14d301']
+    p1, = ax[0].plot(keys, data_time, color=colors[0], linestyle='dashed', marker='^', markeredgecolor='white',
+                   markerfacecolor=colors[0], markersize=8, label="Time in Minutes")
+    p2, = ax[1].plot(keys, data_ram, color=colors[1], linestyle='dashed', marker='o', markeredgecolor='white',
+                   markerfacecolor=colors[1], markersize=8, label="RAM Usage in GiB")
+    p3, = ax[2].plot(keys, data_area, color=colors[2], linestyle='dashed', marker='s', markeredgecolor='white',
+                   markerfacecolor=colors[2], markersize=8, label="Die Area in mm²")
+    
+    ax[0].errorbar(keys, data_time, yerr = (error_time_low, error_time_high), fmt="none", color=colors[0])
+    ax[1].errorbar(keys, data_ram, yerr = (error_ram_low, error_ram_high), fmt="none", color=colors[1])
+    ax[2].errorbar(keys, data_area, yerr = (error_area_low, error_area_high), fmt="none", color=colors[2])
+    
+    ax[0].set(ylabel="Time in Minutes")
+    ax[1].set(ylabel="RAM Usage in GiB")
+    ax[2].set(xlabel="Fabric Size in CLB", ylabel="Die Area in mm²")
+    
+    ax[0].yaxis.label.set_color(p1.get_color())
+    ax[1].yaxis.label.set_color(p2.get_color())
+    ax[2].yaxis.label.set_color(p3.get_color())
+    
+    ax[0].tick_params(axis='y', colors=p1.get_color())
+    ax[1].tick_params(axis='y', colors=p2.get_color())
+    ax[2].tick_params(axis='y', colors=p3.get_color())
+    
+    ax[0].yaxis.set_major_locator(plticker.MultipleLocator(base=5.0))
+    ax[1].yaxis.set_major_locator(plticker.MultipleLocator(base=2.0))
+    ax[2].yaxis.set_major_locator(plticker.MultipleLocator(base=20.0))
+    
+    ax[0].set_ylim([-2.5, 20])
+    ax[1].set_ylim([0, 9])
+    ax[2].set_ylim([-5, 65])
+    
+    ax[0].grid()
+    ax[1].grid()
+    ax[2].grid()
+    
+    fig.align_labels()
+    
+    ax[0].set_xticklabels([])
+    ax[1].set_xticklabels([])
+    
+    ax[0].set_xticks(keys)
+    ax[1].set_xticks(keys)
 
-    ax2 = ax1.twinx()
-    ax3 = ax1.twinx()
-
-    ax2.spines.right.set_position(("axes", 1.0))
-    ax3.spines.right.set_position(("axes", 1.2))
-
-    p1, = ax1.plot(keys, data_time, color=colors[0], linestyle='dashed', marker='^', markeredgecolor='white',
-                   markerfacecolor=colors[0], markersize=12, label="Time in Minutes")
-    p2, = ax2.plot(keys, data_ram, color=colors[1], linestyle='dashed', marker='o', markeredgecolor='white',
-                   markerfacecolor=colors[1], markersize=12, label="RAM Usage in GiB")
-    p3, = ax3.plot(keys, data_area, color=colors[2], linestyle='dashed', marker='s', markeredgecolor='white',
-                   markerfacecolor=colors[2], markersize=12, label="Die Area in mm²")
-    
-    ax1.errorbar(keys, data_time, yerr = error_time, fmt="none", color=colors[0])
-    ax2.errorbar(keys, data_ram, yerr = error_ram, fmt="none", color=colors[1])
-    ax3.errorbar(keys, data_area, yerr = error_area, fmt="none", color=colors[2])
-    
-    ax1.set(xlabel="Fabric Size in CLB", ylabel="Time in Minutes")
-    ax2.set(ylabel="RAM Usage in GiB")
-    ax3.set(ylabel="Die Area in mm²")
-    
-    ax1.yaxis.label.set_color(p1.get_color())
-    ax2.yaxis.label.set_color(p2.get_color())
-    ax3.yaxis.label.set_color(p3.get_color())
-    
-    ax1.tick_params(axis='y', colors=p1.get_color())
-    ax2.tick_params(axis='y', colors=p2.get_color())
-    ax3.tick_params(axis='y', colors=p3.get_color())
-    
     keys_labels = [str(key)+'×'+str(key) for key in keys]
-    plt.xticks(ticks=keys, labels=keys_labels)
-    
-    ax1.legend(handles=[p1, p2, p3], loc='upper left', fontsize="14", fancybox=True)
+    plt.xticks(ticks=keys, labels=keys_labels, rotation=70)
 
-    ax1.set_axisbelow(True)
+    plt.subplots_adjust(hspace=0.13)
+
     plt.show()
 
     os.makedirs('diagrams/', exist_ok=True)
@@ -339,14 +383,16 @@ def print_area():
 
         sti_area = round(float(sti_width) * float(sti_height) / 1000_000, 2)
         
-        print(f'{size} | {sog_area} | {sti_area}')
+        overhead = round((sti_area - sog_area) / sog_area * 100.0, 2)
+        
+        print(f'{size} | {sog_area} | {sti_area} | {overhead}%')
 
 def main():
     load_measurements()
 
     bar_chart_time()
     bar_chart_ram()
-    #diagram_batch()
+    diagram_batch()
     
     print_area()
 
